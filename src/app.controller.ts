db7becb64ba91ef96a53e53e9785d2f4dbf42b78
Augusto <augusporres@gmail.com>
2024-09-18 20:58:46 -0300

    testing swagger docs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {ApiTags, ApiOperation, ApiParam, ApiResponse, ApiBody} from '@nestjs/swagger';
 
-@Controller()
+@ApiTags('Greetings')
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello')
+  @ApiOperation({summary: 'Say Hello to client'})
+  @ApiResponse({ status: 200, description: 'Hello', type: 'string'})
+  @ApiResponse({status: 404, description: 'Endpoint not found'})
   getHello(): string {
     return this.appService.getHello();
   }
