b743bd78a5b509a79cf8bab4e5cc9c71c4e69bbd
Augusto <augusporres@gmail.com>
2024-09-19 00:13:27 -0300

    addin protection to endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserDto } from 'src/user/dto/user.dto';
 import { ApiBody } from '@nestjs/swagger';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -10,7 +11,13 @@ export class AuthController {
     
     @Post('login')
     @ApiBody({type: UserDto})
-    signIn(@Body() signInDto: Record<string, any>) {
+    signIn(@Body() signInDto: UserDto) {
         return this.authService.signIn(signInDto.username, signInDto.password);
+    }
+
+    @UseGuards(AuthGuard)
+    @Get('profile')
+    getProfile(@Request() req) {
+        return req.user
     }
 }
