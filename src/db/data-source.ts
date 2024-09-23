a4be3da19e2ef50881e3c9ff89c338500e2d91e8
Augusto <augusporres@gmail.com>
2024-09-23 13:23:34 -0300

    migrate to postgres

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -2,16 +2,18 @@ import { Role } from "src/auth/entities/role-entity";
 import { Movie } from "src/movie/entities/movie-entity";
 import { User } from "src/user/entities/user.entity";
 import { DataSource, DataSourceOptions } from "typeorm";
+import {config} from 'dotenv';
+config()
 
 export const dataSourceOptions: DataSourceOptions = {
-    type: 'mariadb',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: '',
-    database: 'starwars',
+    type: 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     entities: [User, Movie, Role],
-    synchronize: false,
+    synchronize: true,
 }
 
 const dataSource = new DataSource(dataSourceOptions);
